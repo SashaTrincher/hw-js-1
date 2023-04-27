ad74aa932b57a8cf4852e8717d7727b47d9a89e4
SashaTrincher <oofman@ukr.net>
2023-04-27 17:47:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// 1
+const age = '13';
+console.log(age);
+
+// 2
+const name = 'Oleksandr';
+console.log(name);
+
+// 3
+let isStudent = true;
+console.log(isStudent);
+
+// 4
+let myString = '"You Will Witness True Horror" - Malenia';
+console.log(myString);
+
+// 5 
+let myNumber = 20 + 10;
+console.log(myNumber);
+
+// 6
+const myNull = null;
+console.log(myNull);
+
+// 7
+const userInput = prompt("What is your name?");
+console.log(`${userInput}`);
+
+
+// 8 
+alert('This is a dengerous procedure.');
+
+if (confirm('Are you sure you want to continue?')) {
+    console.log('User clicked OK');
+  } else {
+    console.log('Action is declined');
+  };
+  
+  
